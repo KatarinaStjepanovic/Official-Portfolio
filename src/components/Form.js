aebe7e309a2b2ef40398cd16b4dd8399bd361967
KatarinaStjepanovic <katarina22april@gmail.com>
2025-08-09 18:55:14 +0200

    Form errors

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,6 +3,7 @@ import { send } from 'emailjs-com'
 
 
 function Form() {
+
   const [form, setForm] = useState(
     {
       name: "",
@@ -13,51 +14,58 @@ function Form() {
   )
 
   const [isSent, setSent] = useState(false)
+  const [error, setError] = useState(false)
+  const [clicked, setClicked] = useState(false)
 
   function onChange(e) {
+    if( !Object.values(form).includes("")){
+      setError(false)
+    }
     const { name, value } = e.target
     setForm({ ...form, [name]: value })
   }
 
   function submitForm(e) {
     e.preventDefault()
+    setClicked(true)
+    if (!Object.values(form).includes("")) {
     setSent(true)
-
-    if( !Object.values(form). includes("")){
-send(
-      "service_yyi0la3",
-      "template_form",
-      form,
-      "93q1WUEj0fnbfNw1o"
-    ).then(response => {
-      console.log("Success!", response.status, response.text)
-    }).catch((err => {
-      console.error("Failed", err)
-    }))
-    }else {
+      send(
+        "service_yyi0la3",
+        "template_form",
+        form,
+        "93q1WUEj0fnbfNw1o"
+      ).then(response => {
+        console.log("Success!", response.status, response.text)
+      }).catch((err => {
+        console.error("Failed", err)
+      }))
+    } else {
       console.log("Failed to send")
+      setError(true)
+
     }
-    
+
   }
 
   return (
     isSent ? <div className='sentMessage'>
-      <div style={{padding: "30px", lineHeight : "30px", fontSize: "20px"}}>Thank your for contacting me! Expect my answer in the near future.</div>
+      <div style={{ padding: "30px", lineHeight: "30px", fontSize: "20px" }}>Thank your for contacting me! Expect my answer in the near future.</div>
       <div className='signature'>Katarina</div>
-    </div> : 
-    <form className='form' onSubmit={submitForm}>
-      <div className='formPadding'>
-        <label for="name">Name</label>
-        <input type='text' name='name' onChange={onChange} value={form.name} />
-        <label for="surname">Surname</label>
-        <input type='text' name='surname' onChange={onChange} value={form.surname} />
-        <label for="email">Email</label>
-        <input type='mail' name='email' onChange={onChange} value={form.email} />
-        <label for="message">Message</label>
-        <input className='formDesc' type='text' name='message' onChange={onChange} value={form.message} />
-        <button className='submitBtn'>Submit</button>
-      </div>
-    </form>
+    </div> :
+      <form className='form' onSubmit={submitForm}>
+        <div className='formPadding'>
+          <label for="name">Name</label>
+          <input type='text' name='name' onChange={onChange} value={form.name} style={{border: form.name === "" && clicked && "1px solid red"}} />
+          <label for="surname">Surname</label>
+          <input type='text' name='surname' onChange={onChange} value={form.surname} style={{border: form.surname === ""  && clicked &&  "1px solid red"}} />
+          <label for="email">Email</label>
+          <input type='mail' name='email' onChange={onChange} value={form.email} style={{border: form.email === ""  && clicked && "1px solid red"}} />
+          <label for="message">Message</label>
+          <input className='formDesc' type='text' name='message' onChange={onChange} value={form.message} style={{border: form.message === ""  && clicked && "1px solid red"}} />
+          <button className='submitBtn' style={{backgroundColor: error && "#F52E14", cursor: error && "not-allowed"}}>Submit</button>
+        </div>
+      </form>
   )
 }
 
